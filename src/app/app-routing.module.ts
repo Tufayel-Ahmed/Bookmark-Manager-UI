07d5d737f141ec4e7726bf2e9afa2d25b75fb021
Tufayel-Ahmed <tufayelahmed.edu@gmail.com>
2022-12-31 16:46:19 +0600

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddBookmarkComponent } from './add-bookmark/add-bookmark.component';
+import { BookmarkDetailsComponent } from './bookmark-details/bookmark-details.component';
+import { BookmarkListComponent } from './bookmark-list/bookmark-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'addbookmark', component: AddBookmarkComponent },
+  { path: 'bookmarklist', component: BookmarkListComponent },
+  { path: 'bookmarkdetails', component: BookmarkDetailsComponent },
+  { path: '', redirectTo: '/addbookmark', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddBookmarkComponent } from './add-bookmark/add-bookmark.component';
+import { BookmarkListComponent } from './bookmark-list/bookmark-list.component';
+import { BookmarkDetailsComponent } from './bookmark-details/bookmark-details.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddBookmarkComponent,
+    BookmarkListComponent,
+    BookmarkDetailsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
